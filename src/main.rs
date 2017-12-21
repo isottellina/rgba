bab80800af0f0dd420614a2bc2240673c07cda4a
Louise <julese50@gmail.com>
2017-12-21 22:16:59 +0100

    Modified frontend comportement to correspond to README

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 // Filename: main.rs
 // Author: Louise <louise>
 // Created: Wed Dec  6 12:07:11 2017 (+0100)
-// Last-Updated: Tue Dec 19 23:31:06 2017 (+0100)
+// Last-Updated: Thu Dec 21 22:15:15 2017 (+0100)
 //           By: Louise <louise>
 //
-extern crate minifb;
+#[cfg(feature = "minifb")] extern crate minifb;
 #[cfg(feature = "sdl")] extern crate sdl2;
 
 extern crate clap;
@@ -20,30 +20,31 @@ mod gb;
 
 use clap::{App, Arg};
 use common::{Core, Console, Platform};
-#[cfg(not(feature = "sdl"))] use platform::minifb::MiniFBPlatform;
+#[cfg(feature = "minifb")] use platform::minifb::MiniFBPlatform;
 #[cfg(feature = "sdl")] use platform::sdl::SDLPlatform;
+use platform::dummy::DummyPlatform;
 use gb::Gameboy;
 
 fn main() {
     env_logger::init();
 
     let matches = App::new("rgba").version("0.1")
-            .about("Multi-console emulator")
-            .author("Louise Z.")
-            .arg(Arg::with_name("bios")
-                 .short("b")
-                 .long("bios")
-                 .value_name("BIOS")
-                 .help("Sets the BIOS/bootrom to use")
-                 .required(false))
-            .arg(Arg::with_name("ROM")
-                 .required(true)
-                 .index(1))
-            .arg(Arg::with_name("debug")
-                 .short("d")
-                 .long("debug")
-                 .help("Enables the debugger at start-up"))
-            .get_matches();
+        .about("Multi-console emulator")
+        .author("Louise Z.")
+        .arg(Arg::with_name("bios")
+             .short("b")
+             .long("bios")
+             .value_name("BIOS")
+             .help("Sets the BIOS/bootrom to use")
+             .required(false))
+        .arg(Arg::with_name("ROM")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("debug")
+             .short("d")
+             .long("debug")
+             .help("Enables the debugger at start-up"))
+        .get_matches();
 
     let rom_name = matches.value_of("ROM").unwrap();
     let bios_name = matches.value_of("bios").unwrap();
@@ -56,13 +57,17 @@ fn main() {
             (Console::None, (0, 0, 0))
         };
 
-    #[cfg(feature = "sdl")]
+    #[cfg(all(feature = "sdl", not(feature = "minifb")))]
     let mut platform =
         SDLPlatform::new(parameters.0, parameters.1, parameters.2);
 
-    #[cfg(not(feature = "sdl"))]
+    #[cfg(all(feature = "minifb", not(feature = "sdl")))]
     let mut platform =
         MiniFBPlatform::new(parameters.0, parameters.1, parameters.2);
+
+    #[cfg(all(not(feature = "minifb"), not(feature = "sdl")))]
+    let mut platform =
+        DummyPlatform::new(parameters.0, parameters.1, parameters.2);
     
     match console {
         Console::Gameboy => {
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -3,9 +3,9 @@
 // Filename: mod.rs
 // Author: Louise <louise>
 // Created: Thu Dec 14 22:19:48 2017 (+0100)
-// Last-Updated: Tue Dec 19 23:31:22 2017 (+0100)
+// Last-Updated: Thu Dec 21 22:15:56 2017 (+0100)
 //           By: Louise <louise>
 // 
 pub mod dummy;
-pub mod minifb;
+#[cfg(feature = "minifb")] pub mod minifb;
 #[cfg(feature = "sdl")] pub mod sdl;

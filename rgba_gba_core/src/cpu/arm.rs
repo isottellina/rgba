5e3e7965b7a3377e820a3aa24a81d8b91090129f
Louise <julese50@gmail.com>
2018-01-15 12:16:49 +0100

    [GBA] ALU done

--- rgba_gba_core/src/cpu/arm.rs
+++ rgba_gba_core/src/cpu/arm.rs
@@ -3,20 +3,42 @@
 // Filename: arm.rs
 // Author: Louise <louise>
 // Created: Sat Jan 13 11:44:36 2018 (+0100)
-// Last-Updated: Sat Jan 13 22:43:30 2018 (+0100)
+// Last-Updated: Mon Jan 15 09:58:50 2018 (+0100)
 //           By: Louise <louise>
 // 
 use cpu::ARM7TDMI;
 use io::Interconnect;
 
 impl ARM7TDMI {
+    fn match_condition(&self, instr: u32) -> bool {
+        match instr & 0xF0000000 {
+            0x00000000 => self.zero,
+            0x10000000 => !self.zero,
+            0x20000000 => self.carry,
+            0x30000000 => !self.carry,
+            0x40000000 => self.sign,
+            0x50000000 => !self.sign,
+            0x60000000 => self.overflow,
+            0x70000000 => !self.overflow,
+            0x80000000 => self.carry && !self.zero,
+            0x90000000 => !self.carry && self.zero,
+            0xA0000000 => self.sign == self.overflow,
+            0xB0000000 => self.sign != self.overflow,
+            0xC0000000 => !self.zero && (self.sign == self.overflow),
+            0xD0000000 => self.zero || (self.sign != self.overflow),
+            _ => unreachable!(),
+        }
+    }
+    
     pub fn next_instruction_arm(&mut self, io: &mut Interconnect, instr: u32) {
         let instr_high = (instr & 0x0FF00000) >> 16;
         let instr_low = (instr & 0x000000F0) >> 4;
         
         let function = ARM_INSTRUCTIONS[(instr_high | instr_low) as usize];
 
-        function(self, io, instr);
+        if instr > 0xE0000000 || self.match_condition(instr) {
+            function(self, io, instr);
+        }
     }
 }
 
--- rgba_gba_core/src/cpu/arm_gen.py
+++ rgba_gba_core/src/cpu/arm_gen.py
@@ -3,7 +3,7 @@
 # Filename: arm_gen.py
 # Author: Louise <louise>
 # Created: Sat Jan 13 17:25:38 2018 (+0100)
-# Last-Updated: Sun Jan 14 23:38:56 2018 (+0100)
+# Last-Updated: Mon Jan 15 12:14:49 2018 (+0100)
 #           By: Louise <louise>
 # 
 import pathlib, sys
@@ -30,29 +30,91 @@ def write_op2_imm(file, high, low):
         file.write("\tif rot != 0 { _cpu.carry = (op2 >> 31) != 0; }\n")
 
 def write_op2_reg(file, high, low):
-    pass
+    s = (high & 0x01) != 0
+    shift = (low & 0x6) >> 2
+    
+    file.write("\tlet rm = _cpu.get_register((instr & 0xF) as usize);\n")
+    if (low & 1) == 0: # By immediate
+        file.write("\tlet amount = (instr >> 7) & 0x1f;\n")
+        if shift == 0:
+            file.write("\tlet op2 = if amount == 0 { rm } else {")
+            if s: file.write(" let tmp = rm << (amount - 1); _cpu.carry = (tmp >> 31) != 0; tmp << 1 };\n")
+            else: file.write(" rm << amount };\n")
+        elif shift == 1:
+            file.write("\tlet op2 = if amount == 0 { ")
+            if s:
+                file.write("_cpu.carry = (rm & 0x80000000) != 0; 0 } else { let tmp = rm >> (amount - 1); ")
+                file.write("_cpu.carry = (tmp & 1) != 0; tmp >> 1 };\n")
+            else:
+                file.write("0 } else { rm >> amount };\n")
+        elif shift == 2:
+            file.write("\tlet op2 = if amount == 0 { ")
+            if s:
+                file.write("_cpu.carry = (rm & 0x80000000) != 0; ((rm as i32) >> 31) as u32 } else { ")
+                file.write("let tmp = ((rm as i32) >> (amount - 1)) as u32; _cpu.carry = tmp & 1 != 0; ")
+                file.write("((tmp as i32) >> 1) as u32 };\n")
+            else:
+                file.write("((rm as i32) >> 31 } else { ((rm as i32) >> amount) as u32 };\n")
+        elif shift == 3:
+            file.write("\tlet op2 = if amount == 0 { ")
+            if s:
+                file.write("let tmp = (rm >> 1) | ((_cpu.carry as u32) << 31);  _cpu.carry = (rm & 1) != 0; ")
+                file.write("tmp } else { let tmp = rm.rotate_right(amount); _cpu.carry = (tmp >> 31) != 0; ")
+                file.write("tmp };\n")
+            else:
+                file.write("(rm >> 1) | ((_cpu.carry as u32) << 31) } else { rm.rotate_right(amount) };\n")
+        
+    else: # By register
+        file.write("\tlet amount = _cpu.get_register(((instr >> 8) & 0xF) as usize) & 0xFF;\n")
+        if shift == 0:
+            if s:
+                file.write("\tlet tmp = rm << (amount - 1); _cpu.carry = (tmp >> 31) != 0;\n")
+                file.write("\tlet op2 = tmp << 1;\n")
+            else:
+                file.write("\tlet op2 = rm << amount;\n")
+        elif shift == 1:
+            if s:
+                file.write("\tlet tmp = rm >> (amount - 1); _cpu.carry = (tmp & 1) != 0;\n")
+                file.write("\tlet op2 = tmp >> 1 ;\n")
+            else:
+                file.write("\tlet op2 = rm >> amount;\n")
+        elif shift == 2:
+            if s:
+                file.write("\tlet tmp = ((rm as i32) >> (amount - 1)) as u32; _cpu.carry = tmp & 1 != 0;\n")
+                file.write("\tlet op2 = ((tmp as i32) >> 1) as u32;\n")
+            else:
+                file.write("\tlet op2 = ((rm as i32) >> amount) as u32;\n")
+        elif shift == 3:
+            file.write("\tlet op2 = rm.rotate_right(amount);\n")
+            if s:
+                file.write("\t_cpu.carry = (op2 >> 31) != 0;\n")
+    
     
 def write_alu(file, high, low):
     op = (high & 0x1e) >> 1
     s = (high & 0x01) != 0
     imm = (high & 0x20) != 0
 
+    if op == 5 or op == 6 or op == 7:
+        file.write("\tlet cf = _cpu.carry;\n")
+    
     # Generation op2 code
     if imm:
         write_op2_imm(file, high, low)
     else:
-        file.write("\tlet op2 = 0;")
-        file.write("\tunimplemented!(\"Reg op2 unimplemented\");\n")
-
+        write_op2_reg(file, high, low)
+        
     test = (op & 0xc == 0x8)
         
     if (op & 0xc == 0x8) and not s:
-        file.write("\tunimplemented!(\"Generating bad ALU instruction (s)\");\n")
+        file.write("\tpanic!(\"Generating bad ALU instruction (s)\");\n")
         return
-
+    
     if op != 13 and op != 15:
         file.write("\tlet rn = _cpu.get_register(((instr >> 16) & 0xF) as usize);\n")
-    file.write("\tlet rd = (instr >> 12) & 0xF;\n")
+
+    if not test or s:
+        file.write("\tlet rd = (instr >> 12) & 0xF;\n")
     
     if op == 8 or op == 0: # AND, TST
         file.write("\tlet res = rn & op2;\n")
@@ -65,8 +127,49 @@ def write_alu(file, high, low):
             file.write("\t\t_cpu.carry = rn >= op2;\n")
             file.write("\t\t_cpu.overflow = (rn ^ op2) & (rn ^ res) & 0x80000000 != 0;\n")
             file.write("\t}\n")
+    elif op == 3: # RSB
+        file.write("\tlet res = op2.wrapping_sub(rn);\n")
+        if s:
+            file.write("\tif rd != 15 {\n")
+            file.write("\t\t_cpu.carry = op2 >= rn;\n")
+            file.write("\t\t_cpu.overflow = (rn ^ op2) & (op2 ^ res) & 0x80000000 != 0;\n")
+            file.write("\t}\n")
+    elif op == 11 or op == 4: # ADD, CMN
+        file.write("\tlet res = rn.wrapping_add(op2);\n")
+        if s:
+            file.write("\tif rd != 15 {\n")
+            file.write("\t\t_cpu.carry = rn > res;\n")
+            file.write("\t\t_cpu.overflow = !(rn ^ op2) & (rn ^ res) & 0x80000000 != 0;\n")
+            file.write("\t}\n")
+    elif op == 5: # ADC
+        file.write("\tlet res = rn.wrapping_add(op2).wrapping_add(cf as u32);\n")
+        if s:
+            file.write("\tif rd != 15 {\n")
+            file.write("\t\t_cpu.carry = if cf { rn >= res } else { rn > res };\n")
+            file.write("\t\t_cpu.overflow = !(rn ^ op2) & (rn ^ res) & 0x80000000 != 0;\n")
+            file.write("\t}\n")
+    elif op == 6: # SBC
+        file.write("\tlet res = rn.wrapping_sub(op2).wrapping_sub(cf as u32);\n")
+        if s:
+            file.write("\tif rd != 15 {\n")
+            file.write("\t\t_cpu.carry = if cf { rn > op2 } else { rn >= op2 };\n")
+            file.write("\t\t_cpu.overflow = (rn ^ op2) & (rn ^ res) & 0x80000000 != 0;\n")
+            file.write("\t}\n")
+    elif op == 7: # RSC
+        file.write("\tlet res = op2.wrapping_sub(rn).wrapping_sub(cf as u32);\n")
+        if s:
+            file.write("\tif rd != 15 {\n")
+            file.write("\t\t_cpu.carry = if cf { op2 > rn } else { op2 >= rn };\n")
+            file.write("\t\t_cpu.overflow = (rn ^ op2) & (op2 ^ res) & 0x80000000 != 0;\n")
+            file.write("\t}\n")
+    elif op == 12: # ORR
+        file.write("\tlet res = rn | op2;\n")
     elif op == 13: # MOV
         file.write("\tlet res = op2;\n")
+    elif op == 14: # BIC
+        file.write("\tlet res = rn & !op2;");
+    elif op == 15: # MVN
+        file.write("\tlet res = !op2;");
     else:
         file.write("\tlet res = 0;\n")
         file.write("\tunimplemented!(\"ALU instruction not implemented : {:08x}\", instr);\n")

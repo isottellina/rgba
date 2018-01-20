af56c87c591752aa258ac68f534d3c78eec06caf
Louise <louise@zanier.org>
2018-01-20 18:38:40 +0100

    Changed logger

--- rgba-sdl/src/main.rs
+++ rgba-sdl/src/main.rs
@@ -3,7 +3,7 @@
 // Filename: main.rs
 // Author: Louise <louise>
 // Created: Wed Dec  6 12:07:11 2017 (+0100)
-// Last-Updated: Tue Jan  9 12:51:08 2018 (+0100)
+// Last-Updated: Sat Jan 20 18:38:15 2018 (+0100)
 //           By: Louise <louise>
 //
 extern crate rgba_common;
@@ -11,7 +11,7 @@ extern crate rgba_builder;
 
 extern crate clap;
 #[macro_use] extern crate log;
-extern crate env_logger;
+extern crate simplelog;
 
 extern crate readline;
 extern crate sdl2;
@@ -26,8 +26,6 @@ use rgba_common::Platform;
 use rgba_builder::ConsoleBuilder;
 
 fn main() {
-    env_logger::init();
-
     let matches = App::new("rgba").version("0.1")
         .about("Multi-console emulator")
         .author("Louise Z.")
@@ -44,11 +42,32 @@ fn main() {
              .short("d")
              .long("debug")
              .help("Enables the debugger at start-up"))
+        .arg(Arg::with_name("log")
+             .short("l")
+             .long("log")
+             .takes_value(true)
+             .possible_value("debug")
+             .possible_value("info")
+             .possible_value("warn")
+             .possible_value("error")
+             .default_value("warn")
+             .help("Set the log level"))
         .get_matches();
 
     let rom_name = matches.value_of("ROM").unwrap();
     let bios_name = matches.value_of("bios").unwrap();
     let debug = matches.is_present("debug");
+    let log = matches.value_of("log").unwrap();
+
+    let log_level = match log {
+        "debug" => simplelog::LogLevelFilter::Debug,
+        "info" => simplelog::LogLevelFilter::Info,
+        "warn" => simplelog::LogLevelFilter::Warn,
+        "error" => simplelog::LogLevelFilter::Error,
+        _ => unreachable!(),
+    };
+
+    let _ = simplelog::TermLogger::init(log_level, simplelog::Config::default()).unwrap();
     
     let console = ConsoleBuilder::default()
         .load_bios(bios_name)
